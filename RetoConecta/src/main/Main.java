d2251ce3b3a56bdf1964ac7ac8f495aa59bd8f45
Debetiesto <144771964+Debetiesto@users.noreply.github.com>
2025-09-25 12:38:15 +0200

    Paquete main

---
+++ RetoConecta/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import controlador.Controlador;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.ConvocatoriaExamen;
+import utilidades.Utilidades;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Controlador con = new Controlador();
+        con.iniciar();
+    }
+}
