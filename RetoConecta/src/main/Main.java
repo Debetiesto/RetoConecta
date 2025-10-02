130b5c9b65dd80c7b90b5a56b4a12f0a71b6dd44
MikelRomero <mikelromerohomobono@gmail.com>
2025-10-02 13:24:13 +0200

    Add files via upload

--- RetoConecta/src/main/Main.java
+++ RetoConecta/src/main/Main.java
@@ -1,10 +1,14 @@
+
 package main;
 
 import controlador.Controlador;
+import excepciones.ValidacionException;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws ValidacionException {
         Controlador controlador = new Controlador();
-        controlador.iniciar(); // todo se maneja dentro del controlador
+        controlador.iniciar(); 
     }
+
 }
