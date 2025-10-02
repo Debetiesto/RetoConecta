27e9f56374e273c52f12c0d0324cf727d0257491
MikelRomero <mikelromerohomobono@gmail.com>
2025-10-02 13:24:53 +0200

    Add files via upload

--- RetoConecta/src/modelo/Dificultad.java
+++ RetoConecta/src/modelo/Dificultad.java
@@ -1,7 +1,31 @@
+
 package modelo;
 
 public enum Dificultad {
-    ALTA,
-    MEDIA,
-    BAJA;
+    ALTA("Alta"), MEDIA("Media"), BAJA("Baja");
+    
+    private String nombre;
+
+	Dificultad(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+    public static Dificultad obtenerPorNombre(String nombre) {
+		for (Dificultad n : Dificultad.values()) {
+			if (n.getNombre().equalsIgnoreCase(nombre)) {
+				return n;
+			}
+		}
+		
+		throw new IllegalArgumentException("Nivel no válido: " + nombre);
+
+	}
 }
--- RetoConecta/src/modelo/UnidadDidactica.java
+++ RetoConecta/src/modelo/UnidadDidactica.java
@@ -1,23 +1,15 @@
 package modelo;
 
+import excepciones.ValidacionException;
 import java.util.List;
 
 public class UnidadDidactica {
-
     private int idU;
     private String acronimo;
     private String titulo;
     private String evaluacion;
     private String descripcion;
-    private List<Enunciado> enunciados;
-
-    public UnidadDidactica(List<Enunciado> enunciados) {
-        this.enunciados = enunciados;
-    }
-
-    public UnidadDidactica() {
-
-    }
+    
 
     public int getIdU() {
         return idU;
@@ -59,12 +51,15 @@ public class UnidadDidactica {
         this.descripcion = descripcion;
     }
 
-    public List<Enunciado> getEnunciados() {
-        return enunciados;
+    public void setDatos() throws ValidacionException{
+        
+      //  this.idU = utilidades.Utilidades.leerInt("introduce el ID de la unidad didactica");
+        this.acronimo = utilidades.Utilidades.introducirCadena("introduce el acronimo");
+        this.titulo = utilidades.Utilidades.introducirCadena("introduce el titulo");
+        this.evaluacion = utilidades.Utilidades.introducirCadena("Introduce la evaluacion");
+        this.descripcion = utilidades.Utilidades.introducirCadena("Introduce la descripcion");
     }
-
-    public void setEnunciados(List<Enunciado> enunciados) {
-        this.enunciados = enunciados;
-    }
-
+    
+    
+    
 }
